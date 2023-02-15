9479a74e740cdf345549eaf1b4ab9d96b3b0a76c
Harsh <soni.hs661@gmail.comm>
2023-02-15 13:22:51 +0530

    create history api for member

--- controllers/members.py
+++ controllers/members.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import requests
 from models.members import Members
+from models.transactions import Transactions
 
 def addMember(name,e):
     mem=Members.select(Members.q.email==e)
@@ -8,3 +9,15 @@ def addMember(name,e):
         return "member already exits"
     member=Members(name=name,email=e)
     return "member added successfully"
+
+def history(id):
+    member=Members.selectBy(id=id)
+    arr=[]
+    if(list(member)!=[]):
+        transactions=Transactions.selectBy(member_id=id)
+        if(list(transactions)==[]):
+            return "no transactions of this member"
+        for transaction in transactions:
+            arr.append(Transactions.get_dict(transaction))
+        return {'transactions':arr,'debt':member[0].debt,'total_books':member[0].hasbooks}
+    return "no member with this id "
--- routes/members.py
+++ routes/members.py
@@ -1,8 +1,14 @@
 from flask import Blueprint,request,jsonify
-from controllers.members import addMember
+from controllers.members import addMember,history
 members=Blueprint('members',__name__)
 @members.route('/add',methods=['POST'])
 def add():
     request_data=request.json
     response=addMember(request_data['name'],request_data['email'])
+    return jsonify({"response":response}),200
+
+@members.route('/history',methods=['POST'])
+def view():
+    request_data=request.json
+    response=history(request_data['member_id'])
     return jsonify({"response":response}),200
