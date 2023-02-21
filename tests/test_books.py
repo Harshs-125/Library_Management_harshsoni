c496b8176410f5e1a5ac25653a2d2e2aa927dc12
Harsh <soni.hs661@gmail.comm>
2023-02-21 15:31:06 +0530

    test case for edit book

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 import requests
+from app import Books
 
 def test_add_books(client):
         # res= client.post('/book/add',json={
@@ -20,3 +21,21 @@ def test_add_books(client):
     })
     assert res4.status_code==400
 
+def test_edit_book(client):
+    demoBook=Books(name="demobook",author="demoauthor",available=20,votes=20)
+    demoBook_id=demoBook.id
+    data1={
+        "key":"name",
+        "value":"demoBook2",
+    }
+    data2={
+        "key":"author",
+        "value":"demoauthor2"
+    }
+    res1=client.patch(f'/book/edit-book-data/{demoBook_id}',json=data1)
+    assert res1.status_code==200
+    res2=client.patch(f'/book/edit-book-data/{demoBook_id}',json=data2)
+    assert res2.status_code==200
+    demoBook.delete(demoBook_id)
+    res3=client.patch(f'/book/edit-book-data/{demoBook_id}',json=data1)
+    assert res3.status_code==404
