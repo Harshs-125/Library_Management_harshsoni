61e75aec1361174e4580aa8c4684b9b6bd6f0db4
Harsh <soni.hs661@gmail.comm>
2023-02-22 17:27:23 +0530

    pay debt test case

--- tests/test_members.py
+++ tests/test_members.py
@@ -34,12 +34,12 @@ def test_member_transaction_history(client):
     res2=client.get(f'/member/history/{member_id}')
     assert res2.status_code==404
 
-@pytest.mark.skip
+
 def test_paydebt(client):
     demomember=Members(name="demo",email="demo@123")
     demomember.debt=100
     member_id=demomember.id
-    res1=client.get(f'/member/paydebt/{member_id}',post={"amount":50})
+    res1=client.post(f'/member/paydebt/{member_id}',json={"amount":50})
     assert res1.status_code==200
     demomember.delete(member_id)
 
